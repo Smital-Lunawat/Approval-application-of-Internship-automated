77c3f06a117a02753725a2b1a7ad7b323bdec5ce
Smital-Lunawat <65792778+Smital-Lunawat@users.noreply.github.com>
2020-07-16 16:42:56 +0530

    Add files via upload

---
+++ admin_1.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Internship Homepage</title>
+  <link rel="stylesheet" href="css/finalcss.css">
+  <script src="https://kit.fontawesome.com/dc6b196a84.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+  <!-- <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a href="#" class="navbar-brand mr-3"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+      <h4 style="color:white;">internship</h4>
+      <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ml-auto">
+          <a href="#" class="nav-item nav-link">Logout</a>
+        </div>
+      </div>
+    </div>
+  </nav> -->
+
+  <body class="pink">
+    <nav class="fixed-top navbar-custom navbar navbar-expand-lg ">
+    <a class="navbar-brand" href="#" style="color: #f7f7f7"><b>RAIT Internships</b></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon">
+  <i class="fas fa-bars" style="color:#fff; font-size:28px;"></i>
+  </span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent" >
+      <ul class="navbar-nav mr-auto">
+        </ul>
+      <form class="form-inline my-2 my-lg-0">
+
+        <a href="admin_2.php" class="btn btn-outline-light my-2 my-sm-0 mr-3"><b>Admin</b></a>
+
+      </form>
+     <a href="#"><i class="fa fa-user-circle fa-2x" style="color: #f7f7f7;"></i> </a>
+    </div>
+
+  </nav>
+  <section id=main>
+
+
+    <div class="container-main">
+    	<h1 class="h1_main" align="center" style="color: #666666;" >LISTS OF INTERNSHIPS FOR APPROVAL</h1><br>
+
+
+        &emsp;&emsp;&emsp;&emsp;&nbsp;<button class="btn btn-outline-custom filter-button" data-filter="all" style="border-color:#c80000;">All</button>&nbsp;
+        <button class="btn btn-outline-custom  filter-button" data-filter="Pending" style="border-color:#c80000;">Approval</button>&nbsp;
+        <button class="btn btn-outline-custom  filter-button" data-filter="in-progress" style="border-color:#c80000;">In Progress</button>&nbsp;
+        <button class="btn btn-outline-custom  filter-button" data-filter="Completed" style="border-color:#c80000;">Completed</button>&nbsp;
+        <button class="btn btn-outline-custom  filter-button" data-filter="Rejected" style="border-color:#c80000;">Rejected</button>
+    </div>
+
+    <div class="row">
+
+    <?php
+					include("connect.php");
+          $sql = "SELECT * FROM internship_data";
+          $result = $conn->query($sql);
+
+          if ($result->num_rows > 0) {
+					while($row = $result->fetch_assoc())
+					{
+
+					?>
+            <div class="col-lg-4 col-md-6 pricing-column filter <?php echo $row['approval_status']; ?> <?php echo $row['completion_status']; ?>">
+              <div class="card border-danger" style="max-width: 2000px;">
+                <a style="color:#fff;" href="admin_2.php?id=<?php echo $row['topic'];?>& p_id=<?php echo $row['roll_no'];?>">
+                  <div class="card-header text-#f7f7f7;" style="background-color: #c80000;">
+                    <h3 class="h3">Internship I</h3>
+                  </div>
+                  <div class="card-body">
+                    <p>
+                      <img src="images/internship.jpg" width="102%" height="145%">
+                    </p>
+                    <h3 style="color:#666666">Roll No : <?php echo $row['roll_no']; ?></h3>
+                    <h3 style="color:#666666">Topic : <?php echo $row['topic']; ?></h3>
+                    <h3 style="color:#666666">Year: <?php echo $row['year_new']; ?></h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+
+            <?php
+            }
+
+            } else { echo "0 internships for approval"; }
+            $conn->close();
+            ?>
+    </div>
+
+  </section>
+
+  <script>
+  $(document).ready(function(){
+
+$(".filter-button").click(function(){
+    var value = $(this).attr('data-filter');
+
+    if(value == "all")
+    {
+        //$('.filter').removeClass('hidden');
+        $('.filter').show();
+    }
+    else{
+
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+        $(".filter").not('.'+value).hide();
+        $('.filter').filter('.'+value).show();
+
+    }
+});
+
+});
+</script>
+
+
+</body>
+
+</html>
